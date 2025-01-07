28bb04d152af49218d3a3c5a35579e9cf7b30d6b
samarthb05 <samarth.bhokare@gmail.com>
2025-01-07 11:08:20 +0530

    added comments

--- producer.js
+++ producer.js
@@ -10,7 +10,7 @@ const producer = kafka.producer();
 const run = async () => {
   await producer.connect();
   await producer.send({
-    topic: 'test-topic',
+    topic: 'test-topic', //topic can also added by using docker command in which you can mentioned topic
     messages: [
       { value: 'Hello from developer!' },
       { value: 'Kafka demo with NodeJs!' },
